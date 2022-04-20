758acc0f2f4cb6c32d2c26d8380dec38e422ef34
Sam Grumbles <samgrumbles@yahoo.com>
2022-04-20 14:00:15 -0500

    initial files and structure added

---
+++ Develop/index.js
@@ -0,0 +1,29 @@
+// Steps to Solve
+    // 1. user answers questions
+    // 2. pass answers in the .then()
+    // 3. hand off to generateMarkdown to create MD syntax
+    // 4. pass MD to writeToFile
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    // use fs.writeFile here
+    // How do  i use the markdown file here??
+        // rest operator maybe??
+        // import markdown functions here
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    // write inquirer prompt here
+    // call writeToFile inside .then()
+    // How to send answers from users into markdown??
+        // pass answers to writeToFile here
+}
+
+// Function call to initialize app
+init();
